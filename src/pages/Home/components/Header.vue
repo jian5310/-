6fe9cceb62c16bf133a58c165d9344a0fe1c053d
jian <935248778@qq.com>
2018-09-05 13:56:20 +0800

    zuixin

--- src/pages/Home/components/Header.vue
+++ src/pages/Home/components/Header.vue
@@ -19,10 +19,11 @@ export default{
 </script>
 
 <style lang="stylus" scoped>
+  @import "~style/varibles.styl"
   .header{
      display:flex
      line-height:.86rem
-     background:#00bcd4
+     background:$bgColor
      color:#fff}
   .header-left{
       width:.64rem;
