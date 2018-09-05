aacfa30b9ec0dcb3dfc85d49235f9300187e72c5
jian <935248778@qq.com>
2018-09-05 15:58:20 +0800

    日志

--- src/main.js
+++ src/main.js
@@ -7,9 +7,12 @@ import fastClick from 'fastClick'
 import 'style/reset.css'
 import 'style/border.css'
 import 'style/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/Home/components/swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+   <swiper :options="swiperOption">
+    <!-- slides -->
+     <swiper-slide v-for="item in swiperList" :key="item.id"><img class="swpier-img" :src="item.imgUrl"></swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+   </swiper>
+
+  </div>
+</template>
+
+<script>
+export default{
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/1c/4847ea66072c7b02.jpg_750x200_c32457fb.jpg'
+        },
+        {
+          id: '002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/ce/bca92895556c5002.jpg_750x200_cf56d46c.jpg'
+        }
+      ]
+    }
+  },
+  name: 'HomeSwiper'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper>>> .swiper-pagination-bullet-active
+    background: #ffffff !important
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    .swpier-img
+        width:100%
+</style>
